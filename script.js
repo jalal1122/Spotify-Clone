4cb8f858b326bd8316f1eb9f45c54dc28741796e
muhammaduzair432 <abuzada735@gmail.com>
2025-03-08 03:33:06 +0500

    lets try this fix as well

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ function playSong(song) {
 
 async function getPlaylists() {
   // get the playlists from the server
-  let response = await fetch("./assets/playlists/");
+  let response = await fetch("./assets/Playlists/");
 
   // change the response to text
   let data = await response.text();
