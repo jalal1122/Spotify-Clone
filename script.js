ce01170f0835918dabf68626d3c0fea7b311ba29
muhammaduzair432 <abuzada735@gmail.com>
2025-03-08 01:56:17 +0500

    let see if this one works

--- script.js
+++ script.js
@@ -47,13 +47,13 @@ async function getSongs(playListPath) {
   // check if the playlist path is empty
   if (playListPath === "" || playListPath === undefined) {
     // get the songs from the music
-    response = await fetch(`https://api.github.com/repos/jalal1122/Spotify-Clone/assets/music/`);
+    response = await fetch(`./assets/music/`);
   }
   // if the playlist path is not empty
   else {
     // get the songs from the playlist
     response = await fetch(
-      `${encodeURI(`https://api.github.com/repos/jalal1122/Spotify-Clone/assets/Playlists/${playListPath}/`)}`
+      `${encodeURI(`./assets/Playlists/${playListPath}/`)}`
     );
   }
 
@@ -120,12 +120,12 @@ function playSong(song) {
   // check if the current playlist is empty
   if (currentPlayList === "") {
     // set the song to the music
-    song = `https://api.github.com/repos/jalal1122/Spotify-Clone/assets/music/${song}`;
+    song = `./assets/music/${song}`;
   }
   // if the current playlist is not empty
   else {
     // set the song to the playlist
-    song = `https://api.github.com/repos/jalal1122/Spotify-Clone/assets/Playlists/${currentPlayList}/${song}`;
+    song = `./assets/Playlists/${currentPlayList}/${song}`;
   }
 
   // check if the song is playing
@@ -169,7 +169,7 @@ function playSong(song) {
 
 async function getPlaylists() {
   // get the playlists from the server
-  let response = await fetch("https://api.github.com/repos/jalal1122/Spotify-Clone/assets/Playlists/");
+  let response = await fetch("./assets/Playlists/");
 
   // change the response to text
   let data = await response.text();
